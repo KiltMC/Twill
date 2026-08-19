1ff5c964f2ac170e45b137fc5f795d8a26da1eaf
BluSpring <bluspringindohq@gmail.com>
2026-08-20 02:51:13 +0800

    Update FML to 11.0.17

--- src/main/kotlin/xyz/bluspring/twill/TwillConstants.kt
+++ src/main/kotlin/xyz/bluspring/twill/TwillConstants.kt
@@ -2,5 +2,5 @@ package xyz.bluspring.twill
 
 object TwillConstants {
     // https://projects.neoforged.net/neoforged/fancymodloader
-    const val FML_VERSION = "11.0.16"
+    const val FML_VERSION = "11.0.17"
 }
